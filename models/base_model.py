55527f9f8ffa355dcb095a6f5ac4bce56bac2ec4
AJS0191 <ashtonjamess@live.com>
2022-02-03 18:58:21 -0600

    updating base model; adding tests

--- models/base_model.py
+++ models/base_model.py
@@ -2,20 +2,31 @@
 """this will be base of all classes"""
 
 
+from queue import Empty
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """creates an instance"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs != {}:
+            self.id = kwargs.get('id')
+            self.created_at = \
+                datetime.strptime(kwargs.get('created_at'),
+                                  "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = \
+                datetime.strptime(kwargs.get('updated_at'),
+                                  "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string representation of an instance"""
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return \
+            "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """changes the updated_at attr to current date and time"""
---
+++ tests/test_base_model.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""testing for AirBnB clone"""
+
+
+import unittest
+from models import base_model
+
+# need to test all functions in class BaseClass init, __str__, save, to_dict
+
+class TestBaseModel(unittest.TestCase):
+    def test___init__(self):
+        """tests the creation of an instance of basemodel class"""
+        test1 = base_model()
+        test2 = base_model()
+        self.assertTrue(test1.id != test2.id)
+        self.assertIsNotNone(test1.updated_at)
+        self.assertIsNotNone(test1.created_at)
+    #each time is set correctly (created and updated)
+
+    def test___str__(self):
+        """tests the output of printing an instance"""
+        test1 = base_model()
+        stringT = test1.__str__
+        self.assertTrue(stringT, "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
+        test1.save()
+        self.assertTrue(stringT != test1.__str__)
+
+    def test_save(self):
+        """test that the updated_on date has been changed correctly"""
+        test1 = base_model()
+        stringT = str(test1.updated_at)
+        test1.save()
+        self.assertTrue(stringT != test1.updated_at)
+        # check around for time based testing tips
+    def test_to_dict(self):
+        """test that the correct dictionary output is given"""
+        test1 = base_model()
+        testDict = test1.__dict__
+        testDict['__class__'] = type(test1)
+        testDict[updated_at] = test1.created_at
+        testDict[created_at] = test1.updated_at
