8958f483f17bb867aefeefbd9d473acb50c0db3f
CalicoWhoMan <mikeyshrier@gmail.com>
2022-02-07 23:23:55 -0600

    /usr/bin/python3

--- tests/test_amenity.py
+++ tests/test_amenity.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 """Documentation"""
 
+import unittest
 from models.amenity import Amenity
 
 class Testamenity(unittest.TestCase):
     def test__init__(self):
         test1 = Amenity()
-        setattr(test1, name, "test1")
+        setattr(test1, "name", "test1")
         self.assertTrue(test1.name == "test1")
